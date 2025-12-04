e6664c32479ec24600545f3f926fa36e5571f380
eliora7 <elioral770@gmail.com>
2025-12-04 13:08:31 +0200

    בדיקה

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const handleFinish = () => {
+    if (typeof window.reportComplete === "function") {
+      window.reportComplete();
+    } else {
+      console.log("SCORM לא זמין כרגע");
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <button onClick={handleFinish}>
+          לסיים לומדה
+        </button>
       </header>
     </div>
   );
